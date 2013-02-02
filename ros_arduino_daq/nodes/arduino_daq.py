a979ff82b9feb6a52bc3b2905f84a6ea419e1e99
florisvb <florisvb@gmail.com>
2013-02-01 16:27:55 -0800

    works now

--- ros_arduino_daq/nodes/arduino_daq.py
+++ ros_arduino_daq/nodes/arduino_daq.py
@@ -3,6 +3,8 @@ import roslib
 roslib.load_manifest('ros_arduino_daq')
 import rospy
 
+from optparse import OptionParser
+
 import serial
 import numpy as np
 import pickle
@@ -12,7 +14,9 @@ from std_msgs.msg import *
 
 class Arduino_DAQ(serial.Serial):
     def __init__ (self, pin, topic_name, **kwargs):
+        print 'Initializing arduino, please wait a few seconds...'
         super(Arduino_DAQ,self).__init__(**kwargs)
+        time.sleep(2)
         
         rospy.init_node('arduino_daq', anonymous=True)
         
@@ -23,25 +27,29 @@ class Arduino_DAQ(serial.Serial):
         self.subscriber = rospy.Subscriber(subscriber_name,Int32,self.subscriber_callback)
         self.publisher = rospy.Publisher(topic_name, Float32)
         
+        print 'ready to stream!'
         while not rospy.is_shutdown():
             self.stream_analog_data()
         
     def subscriber_callback(self, data):
-        if data.data:
+        if data.data == 1:
+            print 'streaming!'
+            self.flushInput()
             self.stream = True
             self.time_start = time.time()
-            self.write('[%s,%s]\n'%(1,pin))
-        else:
+            self.write('[%s,%s]\n'%(100,2))
+            self.write('[%s,%s]\n'%(1,self.pin))
+        elif data.data == 0:
+            print 'stopping streaming!'
             self.stream = False
-            self.write('[%s,%s]\n'%(2,pin))
+            self.write('[%s,%s]\n'%(2,self.pin))
                 
     def stream_analog_data(self):
         if self.stream:
             data = self.readline().strip()
             if data is not None:
                 try:
-                    val, micros = data.split(',')
-                    val = int(val)
+                    val = int(data)
                     self.publisher.publish(val)
                 except:
                     print "Bad data -- should be a rare occurence, data looked like: ", data
@@ -50,9 +58,24 @@ class Arduino_DAQ(serial.Serial):
             
                 
 if __name__ == '__main__':
-    pin = 0
-    topic_name = 'arduino_data'
-    daq = Arduino_DAQ(pin, topic_name, port='/dev/ttyACM0',timeout=1, baudrate=115200)
+    
+    parser = OptionParser()
+    parser.add_option("--pin", type="int", dest="pin", default=0,
+                        help="analog pin on arduino to stream")
+    parser.add_option("--port", type="string", dest="port", default=None,
+                        help="port where arduino is connected to, look at /dev/serial/by-id/")
+    parser.add_option("--topic", type="string", dest="topic", default="arduino_data",
+                        help="topic name you would like to publish to")
+    (options, args) = parser.parse_args()
+    
+    topic_control_name = options.topic + '_control'
+    
+    print 
+    print
+    print 'Publishing arduino data from analog pin ', options.pin, '\nto rostopic: ', options.topic, '\nto start streaming from the command line enter: $ rostopic pub', topic_control_name, ' std_msgs/Int32 1 -1 \nto stop streaming: $ rostopic pub', topic_control_name, ' std_msgs/Int32 0 -1'
+    print
+    
+    daq = Arduino_DAQ(options.pin, options.topic, port=options.port,timeout=1, baudrate=115200)
     
     # to start streaming from the command line type:
     #   $ rostopic pub /arduino_data_control std_msgs/Int32 1 -1
